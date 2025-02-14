e1dd04304766706228f9314237046ce7a7761ef9
Vaibhav Gurav <151390343+VaibhavGurav07@users.noreply.github.com>
2025-02-14 10:04:03 +0000

    Create Model

--- employee_management/employee_app/models.py
+++ employee_management/employee_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    lacation = models.CharField(max_length=100)
+
+class Role(models.Model):
+    name = models.CharField(max_length=100,null=False)
+
+class Accesseries(models.Model):
+    name = models.CharField(max_length=50, null= False)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100, null = False)
+    last_name = models.CharField(max_length=100)
+    dept = models.ForeignKey(Department,on_delete = models.CASCADE)
+    salary = models.IntegerField(default= 0)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    phone = models.IntegerField(default=0)
+    hire_date = models.DateField()
+    assign = models.ForeignKey(Accesseries,on_delete=models.CASCADE)
