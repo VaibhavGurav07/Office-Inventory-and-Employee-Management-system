2fade2a4f5d1b426060dd3d0edc79d887d9fd0f8
Vaibhav Gurav <151390343+VaibhavGurav07@users.noreply.github.com>
2025-02-18 11:30:33 +0000

    view all employee page

--- employee_management/employee_app/views.py
+++ employee_management/employee_app/views.py
@@ -4,20 +4,24 @@ from. models import Employee , Role, Department,Accesseries
 # Create your views here.
 
 def index(request):
-    return  render(request,"employee/index.html")
+    return render(request,"employee/index.html")
     
+
 def all_emp(request):
-    emp = employee.objects.all()
+    emp = Employee.objects.all()
     context={
         'emp': emp
     }
     return  render(request,"employee/all_emp.html",context)
 
+
 def add_emp(request):
     return  render(request,"employee/add_emp.html")
 
+
 def remove_emp(request):
     return  render(request,"employee/remove_emp.html")
+
 
 def filter_emp(request):
     return  render(request,"employee/filter_emp.html")
