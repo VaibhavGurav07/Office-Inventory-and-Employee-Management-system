7aa9cbcc451d0e313b1646bc11aa38d0f08022e0
Vaibhav Gurav <151390343+VaibhavGurav07@users.noreply.github.com>
2025-02-18 10:18:40 +0000

    Create Urls and Views

--- employee_management/employee_app/models.py
+++ employee_management/employee_app/models.py
@@ -6,11 +6,20 @@ class Department(models.Model):
     name = models.CharField(max_length=100)
     lacation = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+        
 class Role(models.Model):
     name = models.CharField(max_length=100,null=False)
 
+    def __str__(self):
+        return self.name
+
 class Accesseries(models.Model):
     name = models.CharField(max_length=50, null= False)
+
+    def __str__(self):
+        return self.name
 
 
 class Employee(models.Model):
@@ -23,3 +32,12 @@ class Employee(models.Model):
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
     assign = models.ForeignKey(Accesseries,on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return str(
+            (
+                self.first_name,
+                self.last_name,
+                self.phone
+            )
+        )
--- employee_management/employee_app/views.py
+++ employee_management/employee_app/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-# def index(request):
-#     return  HttpResponse("hello django")
+def index(request):
+    return  render(request,"employee/index.html")
+    
+def all_emp(request):
+    return  render(request,"employee/all_emp.html")
+
+def add_emp(request):
+    return  render(request,"employee/add_emp.html")
+
+def remove_emp(request):
+    return  render(request,"employee/remove_emp.html")
+
+def filter_emp(request):
+    return  render(request,"employee/filter_emp.html")
